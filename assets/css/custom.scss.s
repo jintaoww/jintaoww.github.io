5902fa0f77b4fc2110497fa1affd38d2938ad1de
jintaoww <wang.jintao@connect.um.edu.mo>
2025-06-28 02:07:57 +0800

    theme

--- assets/css/custom.scss.s
+++ assets/css/custom.scss.s
@@ -1,19 +1,31 @@
-/* 明亮主题 */
-body.light-mode {
+// 默认暗色主题
+body.theme-dark {
+  background: #1c1c1c !important;
+  color: #eeeeee !important;
+}
+
+body.theme-dark a {
+  color: #66ccff !important;
+}
+
+body.theme-dark .page__footer,
+body.theme-dark .masthead,
+body.theme-dark .sidebar {
+  background: #222 !important;
+}
+
+// 明亮主题
+body.theme-light {
   background: #ffffff !important;
-  color: #111 !important;
+  color: #111111 !important;
 }
 
-body.light-mode .masthead,
-body.light-mode .page__footer,
-body.light-mode .sidebar {
-  background: #f5f5f5 !important;
-}
-
-body.light-mode a {
+body.theme-light a {
   color: #007acc !important;
 }
 
-body.light-mode .archive__item-title {
-  color: #222 !important;
+body.theme-light .page__footer,
+body.theme-light .masthead,
+body.theme-light .sidebar {
+  background: #f8f8f8 !important;
 }
